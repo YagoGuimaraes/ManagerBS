7df5c73d18249dd8efe1b6d1d5889c9cc0ca077d
YagoGuimaraes <yago.guimaraes@managerbs.com>
2021-01-26 09:13:04 -0300

    Primeiros Passos

---
+++ Exercicio2 - FINAL/Exercicio2 - FINAL/DAL/AlunosContext.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Exercicio2___FINAL.DAL
+{
+    class AlunosContext
+    {
+    }
+}
---
+++ Exercicio2 - FINAL/Exercicio2 - FINAL/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Exercicio2___FINAL
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
